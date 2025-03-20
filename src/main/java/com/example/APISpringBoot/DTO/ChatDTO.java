b87b504f6c1aa74e828a9a92568697850ad340d2
pedro11pucci <pedro11pucci@gmail.com>
2025-03-20 17:57:41 -0300

    :sparkles: feat: chatbot input controller

---
+++ src/main/java/com/example/APISpringBoot/DTO/ChatDTO.java
@@ -0,0 +1,15 @@
+package com.example.APISpringBoot.DTO;
+
+public class ChatDTO {
+    private String text;
+
+    public ChatDTO() {}
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
